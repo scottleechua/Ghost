c9cc246d4c1e4633105b36b28664214081b9d401
scottleechua <scottleechua@gmail.com>
2025-05-05 19:11:26 +0800

    SCOTT: Complete enable-dark-mode script

--- scottleechua/enable-dark-mode.zsh
+++ scottleechua/enable-dark-mode.zsh
@@ -30,6 +30,34 @@ cat > "$TEMP_FILE" << 'EOL'
         }
 EOL
 
+# Create a temporary file with the dark mode styles
+DARK_STYLES_FILE=$(mktemp)
+cat > "$DARK_STYLES_FILE" << 'EOL'
+    html.dark {
+        --black: #fff;
+        --blackrgb: 255,255,255;
+        --grey0: #fbfbfb;
+        --grey1: #f9f9f9;
+        --grey1rgb: 249,249,249;
+        --grey2: #eaeaea;
+        --grey3: #e1e1e1;
+        --grey4: #dcdcdc;
+        --grey5: #c5c5c5;
+        --grey6: #aeaeae;
+        --grey7: #979797;
+        --grey8: #7f7f7f;
+        --grey9: #686868;
+        --grey10: #515151;
+        --grey11: #474747;
+        --grey12: #3d3d3d;
+        --grey13: #333;
+        --grey13rgb: 33,33,33;
+        --grey14: #1d1d1d;
+        --white: #000;
+        --whitergb: 0,0,0;
+    }
+EOL
+
 # Edit PopupModal.js to add dark mode detection
 sed -i '' -e "/const isMobile = window.innerWidth < 480;/r $TEMP_FILE" "$GHOST_ROOT/apps/portal/src/components/PopupModal.js"
 sed -i '' -e "s/dataDir={this.context.dir}/dataDir={this.context.dir}\n                    dataDark={isParentDark}/" "$GHOST_ROOT/apps/portal/src/components/PopupModal.js"
@@ -38,7 +66,17 @@ sed -i '' -e "s/dataDir={this.context.dir}/dataDir={this.context.dir}\n         
 sed -i '' -e "/const {type, status, autoHide, duration} = this.state;/r $TEMP_FILE" "$GHOST_ROOT/apps/portal/src/components/Notification.js"
 sed -i '' -e "s/testid=\"portal-notification-frame\"/testid=\"portal-notification-frame\"\n                    dataDark={isParentDark}/" "$GHOST_ROOT/apps/portal/src/components/Notification.js"
 
-# Clean up temporary file
+# Edit Frame.js to add dark/light class
+sed -i '' -e "s/this.forceUpdate();/this.iframeHtml.classList.add(this.props.dataDark ? 'dark' : 'light');\n            this.forceUpdate();/" "$GHOST_ROOT/apps/portal/src/components/Frame.js"
+
+# Edit Global.styles.js to add dark mode styles
+GLOBAL_STYLES_PATH="$GHOST_ROOT/apps/portal/src/components/Global.styles.js"
+TEMP_GLOBAL_STYLES=$(mktemp)
+awk '/body {/ {system("cat '"$DARK_STYLES_FILE"'"); print; next} {print}' "$GLOBAL_STYLES_PATH" > "$TEMP_GLOBAL_STYLES"
+mv "$TEMP_GLOBAL_STYLES" "$GLOBAL_STYLES_PATH"
+
+# Clean up temporary files
 rm "$TEMP_FILE"
+rm "$DARK_STYLES_FILE"
 
 echo "Dark mode changes applied successfully!" 
